a8793afa77db6e49894ee3c8fcd6c4d0319474c8
Jeronimo Penha <jeronimopenha@gmail.com>
2025-03-27 15:23:44 -0300

    Yoto with LP priority

--- run_reports.sh
+++ run_reports.sh
@@ -3,20 +3,23 @@
 
 # Lista de execuções (nome_base, pasta_base, [num_processos])
 configs=(
-        "too-lrg yott 16"
-        "alu4 yott 16"
-        "apex2 yott 16"
-        "apex4 yott 16"
-        "ex5p yott 16"
-        "misex3 yott 16"
-        "too-lrg yoto_df 16"
-        "alu4 yoto_df 16"
-        "apex2 yoto_df 16"
-        "apex4 yoto_df 16"
-        "ex5p yoto_df 16"
-        "misex3 yoto_df 16"
+#        "too-lrg yott 16"
+#        "alu4 yott 16"
+#        "apex2 yott 16"
+#        "apex4 yott 16"
+#        "ex5p yott 16"
+#        "misex3 yott 16"
+#	"too-lrg yoto_df_io_placed 16"
+#        "alu4 yoto_df_io_placed 16"
+#        "apex2 yoto_df_io_placed 16"
+#        "apex4 yoto_df_io_placed 16"
+#        "ex5p yoto_df_io_placed 16"
+#        "misex3 yoto_df_io_placed 16"
+	"ex1010 yott 16"
+	"ex1010 yoto_df_io_placed 16"
+
 )
-#"ex1010 yott 16"
+
 
 # Caminho para o script principal
 SCRIPT="./run_vpr.sh"
