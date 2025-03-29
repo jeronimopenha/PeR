a3a0998441430c4864b2b73689b86bdcbdd49514
Jeronimo Penha <jeronimopenha@gmail.com>
2025-03-29 19:45:11 -0300

    update run_reports.sh

--- run_reports.sh
+++ run_reports.sh
@@ -3,20 +3,53 @@
 
 # Lista de execuções (nome_base, pasta_base, [num_processos])
 configs=(
-#        "too-lrg yott 16"
-#        "alu4 yott 16"
-#        "apex2 yott 16"
-#        "apex4 yott 16"
-#        "ex5p yott 16"
-#        "misex3 yott 16"
-#	"too-lrg yoto_df_io_placed 16"
-#        "alu4 yoto_df_io_placed 16"
-#        "apex2 yoto_df_io_placed 16"
-#        "apex4 yoto_df_io_placed 16"
-#        "ex5p yoto_df_io_placed 16"
-#        "misex3 yoto_df_io_placed 16"
-	"ex1010 yott 16"
-	"ex1010 yoto_df_io_placed 16"
+        "too-lrg yott 16"
+        "alu4 yott 16"
+        "apex2 yott 16"
+        "apex4 yott 16"
+        "ex5p yott 16"
+        "misex3 yott 16"
+
+#        "too-lrg yott_d 16"
+#        "alu4 yott_d 16"
+#        "apex2 yott_d 16"
+#        "apex4 yott_d 16"
+#        "ex5p yott_d 16"
+#        "misex3 yott_d 16"
+
+#        "too-lrg yott_io 16"
+#        "alu4 yott_io 16"
+#        "apex2 yott_io 16"
+#        "apex4 yott_io 16"
+#        "ex5p yott_io 16"
+#        "misex3 yott_io 16"
+
+#        "too-lrg yoto_df 16"
+#        "alu4 yoto_df 16"
+#        "apex2 yoto_df 16"
+#        "apex4 yoto_df 16"
+#        "ex5p yoto_df 16"
+#        "misex3 yoto_df 16"
+
+#        "too-lrg yoto_df_prio 16"
+#        "alu4 yoto_df_prio 16"
+#        "apex2 yoto_df_prio 16"
+#        "apex4 yoto_df_prio 16"
+#        "ex5p yoto_df_prio 16"
+#        "misex3 yoto_df_prio 16"
+
+#         "too-lrg sa 16"
+#         "alu4 sa 16"
+#         "apex2 sa 16"
+#         "apex4 sa 16"
+#         "ex5p sa 16"
+#         "misex3 sa 16"
+
+#       "ex1010 yott 16"
+#       "ex1010 yoto_df_io_placed 16"
+#        "ex1010 yoto_df_prio 16"
+#        "ex1010 sa 16"
+
 
 )
 
